2031e7b0ccdf52fc277a5710563cd8bc9745059b
ArtemMoiseev <moiseev@physics.msu.ru>
2025-09-17 15:36:25 +0300

    Initial Commit

---
+++ TapinkApp/TapinkAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TapinkAppApp.swift
+//  TapinkApp
+//
+//  Created by Artem on 17.09.2025.
+//
+
+import SwiftUI
+
+@main
+struct TapinkAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
